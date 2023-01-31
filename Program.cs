a7eae1fc3644a9c1ea1a15a56f945680bedeb53c
Elis <eliso1190@yandex.ru>
2023-02-01 00:13:47 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите размер массива : ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandomArray(numbers);
+GetArray(numbers);
+int count = 0;
+
+for (int evennumber = 0; evennumber < numbers.Length; evennumber++)
+if (numbers[evennumber] % 2 == 0)
+count++;
+
+Console.WriteLine($"Чётных" + " " + count);
+
+void RandomArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void GetArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
